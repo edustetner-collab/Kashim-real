9908b34f217a258468497c542034fb561d1735c6
riconessavida-droid <riconessavida@gmail.com>
2026-06-03 11:36:05 -0300

    security: fix Clerk vuln, remove Tailwind CDN + dead packages (stripe, google/genai)

--- index.tsx
+++ index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ClerkProvider } from '@clerk/clerk-react';
 import { ptBR } from '@clerk/localizations';
+import './src/tailwind.css';
 import App from './App';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
